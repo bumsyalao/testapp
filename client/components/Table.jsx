71d8f6f7436d7b882e8a389e92de9088fc293206
bumsyalao <alaobunmi93@gmail.com>
2018-08-12 15:13:39 +0100

    redirect unauthroized users

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -12,23 +12,7 @@ class Table extends Component {
 	constructor(props) {
 		super(props);
 		this.onDelete = this.onDelete.bind(this);
-	}
-	componentDidMount() {
-		const modal = document.getElementById('myModal');
-		const btn = document.getElementById("myBtn");
-		const span = document.getElementsByClassName("close")[0];
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-	}
+  }
 
 	/**
 	 * Function to delete user
