16698e64be418516b7eccb29080aff31fa8c7059
bumsyalao <alaobunmi93@gmail.com>
2018-08-12 15:31:33 +0100

    redirect unauthroized users

--- client/actions/userAction.js
+++ client/actions/userAction.js
@@ -3,102 +3,101 @@ import attachAuthToken from '../utils/attachAuthToken';
 import { LOGIN_USER, REGISTER_USER, LOGOUT_USER, CREATE_USER, GET_USERS } from './types';
 
 export const loginSuccess = (userInfo, message) => ({
-	type: LOGIN_USER,
-	userInfo,
-	message
+  type: LOGIN_USER,
+  userInfo,
+  message
 });
 
 export const registerSuccess = (userInfo, message) => ({
-	type: REGISTER_USER,
-	userInfo,
-	message
+  type: REGISTER_USER,
+  userInfo,
+  message
 });
 
 export const logoutSuccess = () => ({
-	type: LOGOUT_USER
+  type: LOGOUT_USER
 });
 
 export const createUserSuccess = (newUser, message) => ({
-	type: CREATE_USER,
-	newUser,
-	message
+  type: CREATE_USER,
+  newUser,
+  message
 });
 export const loadUsersSuccess = (allUsers, paginate) => ({
-	type: GET_USERS,
-	paginate,
-	allUsers
+  type: GET_USERS,
+  paginate,
+  allUsers
 });
 
 export const loginRequest = loginInfo => dispatch =>
-	axios
-		.post('/api/v1/login', loginInfo)
-		.then((response) => {
-			localStorage.setItem('token', response.data.token);
-			attachAuthToken(response.data.token);
-			return dispatch(
-				loginSuccess(response.data.UserDetails, response.data.message)
-			);
-		}).catch((error) => {
-			throw error;
-		});
-	
+  axios
+    .post('/api/v1/login', loginInfo)
+    .then((response) => {
+      localStorage.setItem('token', response.data.token);
+      attachAuthToken(response.data.token);
+      return dispatch(
+        loginSuccess(response.data.UserDetails, response.data.message)
+      );
+    }).catch((error) => {
+      throw error;
+    });
+
 
 export const registerRequest = registerInfo => dispatch =>
-	axios
-		.post('/api/v1/register', registerInfo)
-		.then((response) => {
-			localStorage.setItem('token', response.data.token);
-			attachAuthToken(response.data.token);
-			return dispatch(
-				registerSuccess(response.data.UserDetails, response.data.message)
-			);
-		})
-		.catch((error) => {
-			throw error;
-		});
+  axios
+    .post('/api/v1/register', registerInfo)
+    .then((response) => {
+      localStorage.setItem('token', response.data.token);
+      attachAuthToken(response.data.token);
+      return dispatch(
+        registerSuccess(response.data.UserDetails, response.data.message)
+      );
+    })
+    .catch((error) => {
+      throw error;
+    });
 
 
 export const logoutRequest = () => (dispatch) => {
-	localStorage.removeItem('token');
-	attachAuthToken(false);
-	return dispatch(logoutSuccess());
+  localStorage.removeItem('token');
+  attachAuthToken(false);
+  return dispatch(logoutSuccess());
 };
 
 export const createUser = newUser => dispatch =>
-	axios
-		.post('/api/v1/createuser', newUser)
-		.then((response) => {
-			return dispatch(
-				createUserSuccess(response.data.newUser, response.data.message)
-			);
-		}).catch((error) => {
-			throw error;
-		});
+  axios
+    .post('/api/v1/createuser', newUser)
+    .then((response) => {
+      return dispatch(
+        createUserSuccess(response.data.newUser, response.data.message)
+      );
+    }).catch((error) => {
+      throw error;
+    });
 
 export const getAllUsers = (selectPage) => dispatch =>
-		axios
-			.get(`/api/v1/users/${selectPage}`)
-			.then((response) => {
-				dispatch(loadUsersSuccess(response.data.allUsers, response.data.paginate ));
-			}).catch((error) => {
-				throw error;
-			});
+  axios
+    .get(`/api/v1/users/${selectPage}`)
+    .then((response) => {
+      dispatch(loadUsersSuccess(response.data.allUsers, response.data.paginate));
+    }).catch((error) => {
+      throw error;
+    });
 
 export const deleteUser = userId => dispatch =>
-			axios
-				.delete(`/api/v1/user/${userId}`)
-				.then(() => {
-					dispatch(getAllUsers());
-				}).catch((error) => {
-					throw error;
-				});
-				
+  axios
+    .delete(`/api/v1/user/${userId}`)
+    .then(() => {
+      dispatch(getAllUsers());
+    }).catch((error) => {
+      throw error;
+    });
+
 export const editUser = (userId, userEmail) => dispatch =>
-			axios
-				.put(`/api/v1/update-user/${userId}`, userEmail)
-				.then(() => {
-					dispatch(getAllUsers());
-				}).catch((error) => {
-					throw error;
-				});
-	
+  axios
+    .put(`/api/v1/update-user/${userId}`, userEmail)
+    .then(() => {
+      dispatch(getAllUsers());
+    }).catch((error) => {
+      throw error;
+    });
--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -9,9 +9,9 @@ class Table extends Component {
 	 * @param {*} props
 	 * @memberof Table
 	 */
-	constructor(props) {
-		super(props);
-		this.onDelete = this.onDelete.bind(this);
+  constructor(props) {
+    super(props);
+    this.onDelete = this.onDelete.bind(this);
   }
 
 	/**
@@ -19,37 +19,37 @@ class Table extends Component {
 	 *
 	 * @memberof Table
 	 */
-	onDelete() {
-		const userId = this.props.id;
-		this.props.deleteUser(userId);
-	}
+  onDelete() {
+    const userId = this.props.id;
+    this.props.deleteUser(userId);
+  }
 	/**
 	 * Render Table component
 	 *
 	 * @returns
 	 * @memberof Table
 	 */
-	render() {
-		const {
-			email,
-			admin,
-			id
-		} = this.props;
-		return (
-			<tr className="table-row">
-				<td>{email} {admin ? <div className="chip">Admin</div> : null}</td>
-				<td>
-					{admin ? <a className="btn disabled">Edit</a> : <Link 
-						className="btn"
-						to={`/update-user/${id}`}>
-						Edit</Link>
-					}
-				</td>
-				<td>{admin ? <a className="btn disabled">Delete</a> : <a className="delete-btn btn" onClick={this.onDelete}>Delete</a>}</td>
-			</tr>
+  render() {
+    const {
+      email,
+      admin,
+      id
+    } = this.props;
+    return (
+      <tr className="table-row">
+        <td>{email} {admin ? <div className="chip">Admin</div> : null}</td>
+        <td>
+          {admin ? <a className="btn disabled">Edit</a> : <Link
+            className="btn"
+            to={`/update-user/${id}`}>
+            Edit</Link>
+          }
+        </td>
+        <td>{admin ? <a className="btn disabled">Delete</a> : <a className="delete-btn btn" onClick={this.onDelete}>Delete</a>}</td>
+      </tr>
 
-		);
-	}
+    );
+  }
 }
 
 export default connect(null, { deleteUser })(Table);
